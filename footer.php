eb31988f9468ce6370b87a68bd6ab51fe24991d7
Mzer Emmanuel Terdoo <mzerterdoo6@gmail.com>
2023-12-16 07:11:33 +0100

    added scresndshot

---
+++ footer.php
@@ -0,0 +1,14 @@
+<footer class="footer text-center py-2 theme-bg-dark">
+
+    <p class="copyright"><a href="https://nowhere.com">Nowhere</a></p>
+    <?php
+    dynamic_sidebar('footer-1');
+    ?>
+</footer>
+
+<?php
+wp_footer();
+?>
+</body>
+
+</html>
